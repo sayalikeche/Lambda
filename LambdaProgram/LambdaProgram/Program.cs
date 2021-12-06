699ccd046397e005cc70936d0fd729b90ec8b165
sayalikeche <kechesayali11@gmail.com>
2021-12-06 21:54:40 +0530

    [sayali Added Create a List]

---
+++ LambdaProgram/LambdaProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Lambda Program");
+        }
+    }
+}
